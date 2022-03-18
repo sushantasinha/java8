f722696c51e08bf2d4d9a2355fca25bd7f27b7b8
sushantasinha <25884819+sushantasinha@users.noreply.github.com>
2022-03-18 17:48:30 +0530

    Runnable interface

---
+++ src/com/learn/java/RunnableLambdaExample.java
@@ -0,0 +1,25 @@
+package com.learn.java;
+
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+
+        //old java approach
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("I am in old approach");
+            }
+        };
+
+        new Thread(runnable).start();
+
+        //new Approach
+        Runnable runnable1 = () -> System.out.println("I am in new approach 1");
+        new Thread(runnable1).start();
+
+        //new Approach 2
+        new Thread(() -> System.out.println("I am in new approach 2")).start();
+
+    }
+}
